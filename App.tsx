5ab2b97f5e5c4099bb6330d2a1a5e17141f2ce4a
AmandaCleto <amanda.clet@gmail.com>
2020-03-29 20:32:05 -0300

    Criado statusBar global

--- App.tsx
+++ App.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
 
 import Register from './src/screens/register/Register';
 import Perfil_Edit from './src/screens/perfil_edit/Perfil_Edit'
+import Perfil from './src/screens/perfil/Perfil'
+
+import Style from './style'
 
 export default function App() {
   return (
     <View>
-      <Perfil_Edit/>
+      <View style={Style.statusBar}></View>
+      <Register/>
     </View>
   );
 }
---
+++ style.tsx
@@ -0,0 +1,12 @@
+import {StyleSheet} from 'react-native';
+import Constants from 'expo-constants';
+
+const style = StyleSheet.create({
+    statusBar: {
+        backgroundColor: '#232F40',
+        height: Constants.statusBarHeight,
+    },
+});
+
+export default style
+
