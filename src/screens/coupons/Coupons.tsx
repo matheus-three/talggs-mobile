7d209c50b44c76abb83040890abc21beefe32b8f
AmandaCleto <amanda.clet@gmail.com>
2020-04-08 19:28:34 -0300

    ajustes

--- src/screens/coupons/Coupons.tsx
+++ src/screens/coupons/Coupons.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React from 'react'
 import { View, Text, TouchableOpacity, Clipboard, Alert, ScrollView } from 'react-native'
 import Ionicons from 'react-native-vector-icons/Ionicons';
 
--- src/screens/register/Register.tsx
+++ src/screens/register/Register.tsx
@@ -17,7 +17,6 @@ const Register = () => {
 		{value: 'AC'}, {value: 'AL'}, {value: 'AP'}, {value: 'AM'}, {value: 'BA'}, {value: 'CE'}, {value: 'DF'}, {value: 'ES'}, {value: 'GO'}, {value: 'MA'}, {value: 'MT'}, {value: 'MS'}, {value: 'MG'}, {value: 'PA'}, {value: 'PB'}, {value: 'PR'}, {value: 'PE'}, {value: 'PI'}, {value: 'RJ'}, {value: 'RN'}, {value: 'RS'}, {value: 'RO'}, {value: 'RR'}, {value:'SC'},{value: 'SP'}, {value: 'SE'}, {value: 'TO'}
 	];
 
-
 	const changeDate = value => {
 		setDate(value);
 	}
